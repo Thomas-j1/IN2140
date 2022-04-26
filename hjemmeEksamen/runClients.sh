d0592a68a03bb76dfbd7a57486df1ab337f8ca08
Thomas Johannessen <thomas.johanness1@gmail.com>
2022-04-26 12:21:03 +0200

    client args

--- hjemmeEksamen/runClients.sh
+++ hjemmeEksamen/runClients.sh
@@ -1,4 +1,4 @@
 for i in {1..5}
 do
-    gnome-terminal --tab --title="client $i" -- bash -c "./upush_client 555$i 'PKT 0 REG name$i';"
+    gnome-terminal --tab --title="client $i" -- bash -c "./upush_client name$i 127.0.0.1 4321 10 10;"
 done
--- hjemmeEksamen/upush_client.c
+++ hjemmeEksamen/upush_client.c
@@ -1,26 +1,61 @@
 #include "common.h"
 
+#define MAXMSGSIZE 1400
+
+int packetNumber = 0;
+
+void register_with_server(const char *nick, int so, struct sockaddr_in dest_addr)
+{
+    char buf[BUFSIZE];
+    int rc;
+
+    sprintf(buf, "PKT %d REG %s", packetNumber++, nick);
+
+    rc = sendto(so,
+                buf,
+                strlen(buf),
+                0,
+                (struct sockaddr *)&dest_addr,
+                sizeof(struct sockaddr_in));
+    check_error(rc, "sendto");
+}
+
+void handle_stdin(int so, struct sockaddr_in dest_addr)
+{
+    char buf[MAXMSGSIZE]; // + strlen(nick)
+    int rc;
+    fgets(buf, BUFSIZE, stdin);
+    buf[strcspn(buf, "\n")] = 0; // if /n overwrite with 0
+    rc = sendto(so, buf, strlen(buf), 0, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in));
+    check_error(rc, "sendto");
+}
+
 int main(int argc, char const *argv[])
 {
     int so, rc;
     unsigned short port;
     char buf[BUFSIZE];
+    char const *nick, *server_ip, *timeout;
     fd_set set;
     struct in_addr ip_addr;
     struct sockaddr_in my_addr, dest_addr;
 
-    if (argc < 2)
+    if (argc < 6)
     {
-        printf("Usage: ./upush_client <port>\n");
+        printf("Usage: ./upush_client <nick> <ip-address> <port> <timeout> <loss_probability>\n");
         return EXIT_SUCCESS;
     }
 
-    port = atoi(argv[1]);
+    nick = argv[1];
+    server_ip = argv[2];
+    port = atoi(argv[3]);
+    timeout = argv[4];
+    setup_loss_probability(argv[5]);
 
     so = socket(AF_INET, SOCK_DGRAM, 0);
     check_error(so, "socket");
 
-    rc = inet_pton(AF_INET, "127.0.0.1", &ip_addr);
+    rc = inet_pton(AF_INET, server_ip, &ip_addr);
     check_error(rc, "inet_pton");
     if (rc == 0)
     {
@@ -29,28 +64,22 @@ int main(int argc, char const *argv[])
     }
 
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(port);
+    my_addr.sin_port = htons(0); // os assigned port
     my_addr.sin_addr = ip_addr;
 
     dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(4321);
+    dest_addr.sin_port = htons(port);
     dest_addr.sin_addr = ip_addr;
 
     rc = bind(so, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
     check_error(rc, "bind");
 
-    strcpy(buf, argv[2]);
-    rc = sendto(so,
-                buf,
-                strlen(buf),
-                0,
-                (struct sockaddr *)&dest_addr,
-                sizeof(struct sockaddr_in));
-    check_error(rc, "sendto");
+    register_with_server(nick, so, dest_addr);
 
     FD_ZERO(&set);
     printf("Welcome to msn. Write quit to leave\n\n");
 
+    memset(buf, 0, BUFSIZE);
     while (strcmp(buf, "q"))
     {
         FD_SET(STDIN_FILENO, &set);
@@ -67,6 +96,7 @@ int main(int argc, char const *argv[])
         }
         else if (FD_ISSET(so, &set))
         {
+            // handle_socket();
             rc = read(so, buf, BUFSIZE - 1);
             check_error(rc, "read");
             buf[rc] = '\0';
